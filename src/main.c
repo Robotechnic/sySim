62a561e493b908765277368e387c12eac4b6097f
Robotechnic <robotechnic.developeur@gmail.com>
2023-12-10 20:18:32 +0100

    Add command line parsing and clang format

--- src/main.c
+++ src/main.c
@@ -1,6 +1,14 @@
+#include "argparse/argparse.h"
 #include <stdio.h>
+#include <stdlib.h>
 
-int main() {
-    printf("Hello, World!\n");
+int main(int argc, char **argv) {
+    struct gengetopt_args_info args_info;
+
+    if (cmdline_parser(argc, argv, &args_info) != 0) {
+        exit(1);
+    }
+
+    cmdline_parser_free(&args_info);
     return 0;
 }
